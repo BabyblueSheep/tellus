fdb0cf4828b51ab0a2470f48f4391818a7df288f
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2025-12-20 16:47:13 +0200

    Update Vector2Extensions.cs

--- src/Tellus/Math/Vector2Extensions.cs
+++ src/Tellus/Math/Vector2Extensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,16 @@ namespace Tellus.Math;
 
 public static class Vector2Extensions
 {
+    public static Vector2 FromAngle(this float angle)
+    {
+        return new Vector2(MathF.Cos(angle), MathF.Sin(angle));
+    }
+
+    public static float ToAngle(this Vector2 vector)
+    {
+        return MathF.Atan2(vector.Y, vector.X);
+    }
+
     public static bool HasNaNs(this Vector2 vector)
     {
         return float.IsNaN(vector.X) || float.IsNaN(vector.Y);
