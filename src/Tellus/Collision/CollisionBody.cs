c12c27c8ef009ca5fad3aa51319820cfc26bf6fd
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2026-01-09 01:14:07 +0200

    im documenting this crap

--- src/Tellus/Collision/CollisionBody.cs
+++ src/Tellus/Collision/CollisionBody.cs
@@ -6,11 +6,24 @@ using System.Collections;
 
 namespace Tellus.Collision;
 
+/// <summary>
+/// A "body" composed of several polygons. Used for collision detection.
+/// </summary>
+/// <remarks>
+/// All vertices are in local space. An offset is stored and applied to go into world space.
+/// </remarks>
 public sealed class CollisionBody : IEnumerable<CollisionPolygon>
 {
+    /// <summary>
+    /// An offset to be applied to vertices.
+    /// </summary>
     public Vector2 Offset { get; set; }
+
     private readonly List<CollisionPolygon> _polygons;
 
+    /// <summary>
+    /// A radius that approximates the size of the body. Used for broad collision.
+    /// </summary>
     public float BroadRadius { get; private set; }
 
     public CollisionBody()
@@ -20,13 +33,16 @@ public sealed class CollisionBody : IEnumerable<CollisionPolygon>
 
     public CollisionBody(params CollisionPolygon[] polygons) : this()
     {
-        
         foreach (var polygon in polygons)
         {
             Add(polygon);
         }
     }
 
+    /// <summary>
+    /// Adds a polygon to the body.
+    /// </summary>
+    /// <param name="polygon">The polygon.</param>
     public void Add(CollisionPolygon polygon)
     {
         _polygons.Add(polygon);
@@ -37,12 +53,41 @@ public sealed class CollisionBody : IEnumerable<CollisionPolygon>
         }
     }
 
+    /// <summary>
+    /// Removes a polygon to the body.
+    /// </summary>
+    /// <param name="polygon">The polygon.</param>
+    public void Remove(CollisionPolygon polygon)
+    {
+        var removed = _polygons.Remove(polygon);
+
+        BroadRadius = 0;
+        if (removed)
+        {
+            foreach (var presentPolygon in _polygons)
+            {
+                foreach (var vertex in presentPolygon.Vertices)
+                {
+                    BroadRadius = MathF.Max(BroadRadius, vertex.Length());
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Clears all polygons from the body.
+    /// </summary>
     public void Clear()
     {
         _polygons.Clear();
         BroadRadius = 0;
     }
 
+    /// <summary>
+    /// Determines whether the this body and another one are within range for narrow collision.
+    /// </summary>
+    /// <param name="otherBody">The other body.</param>
+    /// <returns>Whether the bodies are within range for narrow collision.</returns>
     public bool IsWithinNarrowRange(CollisionBody otherBody)
     {
         var combinedBroadRadius = this.BroadRadius + otherBody.BroadRadius;
--- src/Tellus/Collision/CollisionPolygon.cs
+++ src/Tellus/Collision/CollisionPolygon.cs
@@ -4,16 +4,33 @@ using Tellus.Math.Shapes;
 
 namespace Tellus.Collision;
 
+/// <summary>
+/// A convex polygon composed of vertices. Used for collision detection.
+/// </summary>
 public sealed class CollisionPolygon
 {
     private readonly Vector2[] _vertices;
     private readonly (Vector2, Vector2)[] _sides;
     private readonly Vector2[] _normals;
 
+    /// <summary>
+    /// The vertices of the polygon.
+    /// </summary>
     public ReadOnlySpan<Vector2> Vertices => new(_vertices);
+    /// <summary>
+    /// The sides of the polygon.
+    /// </summary>
     public ReadOnlySpan<(Vector2, Vector2)> Sides => new(_sides);
+    /// <summary>
+    /// The normalized normals of the polygon.
+    /// </summary>
     public ReadOnlySpan<Vector2> Normals => new(_normals);
 
+    /// <summary>
+    /// Constructs a polygon that approximates a circle.
+    /// </summary>
+    /// <param name="circle">The parameters of a circle to use.</param>
+    /// <param name="vertexCount">The amount of vertices to use. Can not go below 3.</param>
     public CollisionPolygon(Circle circle, uint vertexCount = 16)
     {
         vertexCount = uint.Max(vertexCount, 3);
@@ -29,6 +46,10 @@ public sealed class CollisionPolygon
         FormSides();
     }
 
+    /// <summary>
+    /// Constructs a polygon that represents a rotateable rectangle.
+    /// </summary>
+    /// <param name="rectangle">The parameters of a rectangle to use.</param>
     public CollisionPolygon(Math.Shapes.Rectangle rectangle)
     {
         var sine = (float)System.Math.Sin(rectangle.Angle);
@@ -57,6 +78,10 @@ public sealed class CollisionPolygon
         FormSides();
     }
 
+    /// <summary>
+    /// Constructs a polygon that represents a triangle.
+    /// </summary>
+    /// <param name="triangle">The parameters of a triangle to use.</param>
     public CollisionPolygon(Triangle triangle)
     {
         _vertices = new Vector2[3];
